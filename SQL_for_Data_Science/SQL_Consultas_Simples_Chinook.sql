43d4c71d5a74acf2932d6295a7bd38adcbdfb6ca
Alejandro Zavala <alejandro.zavala@gmail.com>
2022-05-07 22:40:34 -0500

    Se sube primer analisis de consultas a base de datos Chinok

---
+++ SQL_for_Data_Science/SQL_Consultas_Simples_Chinook.sql
@@ -0,0 +1,19 @@
+-- Consultas simples para la base de datos Chinook
+
+-- Mostrar toda la información de la tabla tracks
+
+Select *
+From Tracks;
+
+-- Mostrar toda la información de la tabla Invoices
+
+Select *
+From Invoices;
+
+-- Mostrar solo los campos Playlistid,Name de la tabla Playlists
+
+Select 
+Playlistid,
+Name
+From Playlists;
+
